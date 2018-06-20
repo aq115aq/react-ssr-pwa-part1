67603df2bfc9997ff3d7f767afd88c68fe9e46c1
张凌枫 <aq115aq@163.com>
2018-06-20 18:11:12 +0800

    no message

--- server/index.js
+++ server/index.js
@@ -2,17 +2,9 @@ import express from 'express'
 import * as fs from 'fs'
 import * as path from 'path'
 
-// // react
-// import * as React from 'react'
-// import * as ReactDOMServer from 'react-dom/server'
-
-// // router
-// import { StaticRouter } from 'react-router-dom'
-// import getRoutes from 'routes/routes';
-
 const app = express()
 
-app.use(express.static(path.resolve('build'), {index: false}))
+app.use(express.static(path.resolve('build'), {index: false})) // 资源获取路径设置为 build 目录下
 
 app.get('*', (req, res) => {
   let html = fs.readFileSync(path.resolve('build/index.html'), 'utf8')
